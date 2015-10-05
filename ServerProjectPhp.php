310c91c3de0614f5ad21f64c6b7ae3a7c8168e3f
yohani78 <yohani78@yahoo.com>
2015-10-05 21:02:18 +0000

    pushing project

--- ServerProjectPhp.php
+++ ServerProjectPhp.php
@@ -2,17 +2,39 @@
 
 // Declared Variables to be use.
 
-        $name = 'name'; //string
+        $name = 'name';         // string
         $lastName = 'lastName'; // string
-        $phoneNumber = ; // int
-        $e-mail = "";
-        $Address = "address";
-        $city = "city";
-        $state = "state";
-        $zipCode ="zipCode";
+        $phoneNumber = 0;       // int
+        $e_mail = 'e_mail';     // string
+        $Address = 'address';   // string
+        $city = 'city';         // string
+        $state = 'state';       // string
+        $zipCode = 0;           // int
         
         
-        function inputName()= ;
+        // function inputName() = ;{
+            
+            
+        
+         <form action="welcome_get.php" method="get">
+                $name: <input type="text" name="name"><br>
+                $lastName: <input type="text" name="lastName"><br>
+                $phoneNumber: <input type="number_format" name="phoneNumber"><br>
+                $e_mail: <input type="text" name="e_mail"><br>
+                $Address: <input type="text" name="address"><br>
+                $city: <input type="text" name="city"><br>
+                $state: <input type="text" name="state"><br>
+                $zipcode: <input type="text" name="zipcode"><br>
+                
+                <input type="submit">
+                
+        </form>}
+        
+        
+        
+        
+        
+        
         
         
         
