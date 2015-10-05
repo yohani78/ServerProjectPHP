1cd5231c4ee41068aa9c121d6e30851f05df5cbc
yohani78 <yohani78@yahoo.com>
2015-10-05 20:45:02 +0000

    pushing project

--- ServerProjectPhp.php
+++ ServerProjectPhp.php
@@ -1,4 +1,21 @@
-<?php // Server Project PHP
+<?php // Server Project PHP - Contact Record
+
+// Declared Variables to be use.
+
+        $name = 'name'; //string
+        $lastName = 'lastName'; // string
+        $phoneNumber = ; // int
+        $e-mail = "";
+        $Address = "address";
+        $city = "city";
+        $state = "state";
+        $zipCode ="zipCode";
+        
+        
+        function inputName()= ;
+        
+        
+        
 
 
 ?>
